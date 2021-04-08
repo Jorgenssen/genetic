822af818714822281f0a19aadad55446afb80d3b
Jorgenssen <n.tatyannikov@gmail.com>
2021-04-08 23:55:56 +0100

    Update logging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,37 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
 
 public class Main {
 
-    public static int getMutatedIndex(int genomLength) {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    private static int getMutatedIndex(int genomLength) {
         return RandomUtils.nextInt(0, genomLength);
     }
 
-    public static char doMutatedGen(char genToMutate) {
+    private static char doMutatedGen(char genToMutate) {
         return RandomStringUtils.random(1, true, false).charAt(0);
     }
 
-    public static String doMutateGenom(String previousGenom, int mutatedIndex, char mutatedGen) {
+    private static String doMutateGenom(String previousGenom, int mutatedIndex, char mutatedGen) {
         return previousGenom.substring(0, mutatedIndex) + mutatedGen + previousGenom.substring(mutatedIndex + 1);
     }
 
-    public static String doMutation(String previousGenom, int genomLength) {
+    private static String doMutation(String previousGenom, int genomLength) {
         int mutatedIndex = getMutatedIndex(genomLength);
         char genToMutate = previousGenom.charAt(mutatedIndex);
         char mutatedGen = doMutatedGen(genToMutate);
 
-        System.out.println("Gen to mutate is \'" + genToMutate + "\' at " + mutatedIndex + " position");
-        System.out.println("Mutated gen: \'" + mutatedGen + "\'");
+        log.info("Gen to mutate is \'{}\' at {} position", genToMutate,  mutatedIndex);
+        log.info("Mutated gen: \'{}\'", mutatedGen);
 
         return doMutateGenom(previousGenom, mutatedIndex, mutatedGen);
     }
 
     public static void main(String[] args) {
-        String preGenom, nextGenom = null;
+        String preGenom, nextGenom;
         int offspringsCount = 10;
         Offspring[] offsprings = new Offspring[offspringsCount];
 
@@ -35,21 +39,22 @@ public class Main {
         int genomLength = initPopulation.getGenom().length();
         preGenom = initPopulation.getGenom();
 
-        System.out.println("------------");
-        System.out.println("Initial population: " + initPopulation.getGenom());
-        System.out.println("Genom length: " + genomLength);
-        System.out.println("Count of offsprings: " + offspringsCount);
-        System.out.println("------------\n");
-        System.out.println("Offsprings:");
+        log.info("------------");
+        log.info("Initial population: {}", initPopulation.getGenom());
+        log.info("Genom length: {}", genomLength);
+        log.info("Count of offsprings: {}", offspringsCount);
+        log.info("------------");
+        log.info("Offsprings:");
 
         for (int i = 0; i < offspringsCount; i++) {
-            System.out.println(i+1);
+            log.info("Population " + i+1);
             nextGenom = doMutation(preGenom, genomLength);
             offsprings[i] = new Offspring(nextGenom);
             offsprings[i].setPreGenom(preGenom);
-            System.out.println(offsprings[i] + " initial genom: " + initPopulation.getGenom());
-            System.out.println(offsprings[i] + " previous genom: " + offsprings[i].getPreGenom());
-            System.out.println(offsprings[i] + " actual genom: " + offsprings[i].getGenom() + "\n");
+            log.info("{} initial genom: {}", offsprings[i], initPopulation.getGenom());
+            log.info("{} previous genom: {}", offsprings[i], offsprings[i].getPreGenom());
+            log.info("{} actual genom: {}", offsprings[i], offsprings[i].getGenom());
+            log.info("------------");
         }
     }
 }
